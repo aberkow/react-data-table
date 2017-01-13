88da011da8cddc71df82525df27ba8a85fa9ef81
Berkowitz <adb16130@d71h90.pr.uconn.edu>
2017-01-13 08:50:11 -0500

    griddle table works w test data moving to next part of project.

--- components/GriddleTable.jsx
+++ components/GriddleTable.jsx
@@ -12,7 +12,16 @@ export default class GriddleTable extends Component {
   }
   render() {
     return (
-      <Griddle results={this.state.mockData} />
+      <Griddle
+        results={this.state.mockData}
+        tableClassName="table"
+        showFilter={true}
+        showSettings={true}
+        columns={ ['first_name', 'last_name', 'affiliation', 'subject']} enableInfiniteScroll={true}
+        useFixedHeader={true}
+        bodyHeight={400}
+        // useGriddleStyles={false}
+      />
     );
   }
 };
